94e44ad2775ca4e3a5b32607bf794dfd45c8f597
Girish Khorate <khorategirish09@gmail.com>
2024-04-11 10:19:35 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MyConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("My First Program!");
+
+
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+}
