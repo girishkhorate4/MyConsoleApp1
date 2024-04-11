63519e44668e2ddee84c2c1db495450b7521aecc
Girish Khorate <khorategirish09@gmail.com>
2024-04-11 10:36:22 +0530

    Added meta files readme and gitignore

--- obj/Debug/net8.0/MyConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94e44ad2775ca4e3a5b32607bf794dfd45c8f597")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
